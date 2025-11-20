d49ff8eb45a848a70953983502c86c34660afd79
luzilong <luzilong2005@foxmail.com>
2025-11-20 14:46:50 +0800

    更新Vite配置

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SITE_BASE_URL: string;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) as unknown as ImportMetaEnv;
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: 55011,
-  },
-  base: "/luzilong2005.me/",
+    server: {
+      port: 55011,
+    },
+    base: env.VITE_SITE_BASE_URL,
+  };
 });
